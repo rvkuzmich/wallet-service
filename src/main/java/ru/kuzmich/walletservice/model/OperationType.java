4637b8c44d21281595fd4584a8d58901a9f87b1f
Roman Kuzmich <rvku2mich@yandex.ru>
2026-01-30 12:14:06 +0500

    OperationType and Wallet models added

---
+++ src/main/java/ru/kuzmich/walletservice/model/OperationType.java
@@ -0,0 +1,6 @@
+package ru.kuzmich.walletservice.model;
+
+public enum OperationType {
+  DEPOSIT,
+  WITHDRAW,
+}
---
+++ src/main/java/ru/kuzmich/walletservice/model/Wallet.java
@@ -0,0 +1,57 @@
+package ru.kuzmich.walletservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "wallets")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Wallet {
+
+  @Id
+  @Column(name = "id", nullable = false, updatable = false)
+  private UUID id;
+
+  @Column(name = "balance", nullable = false, precision = 15, scale = 2)
+  private BigDecimal balance;
+
+  @Version
+  @Column(name = "version", nullable = false)
+  private Long version;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  public void deposit(BigDecimal amount) {
+    this.balance = this.balance.add(amount);
+  }
+
+  public void withdraw(BigDecimal amount) {
+    if (this.balance.compareTo(amount) < 0) {
+      throw new IllegalArgumentException("Insufficient funds");
+    }
+    this.balance = this.balance.subtract(amount);
+  }
+}
