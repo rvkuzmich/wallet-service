9c1781a986e732c0edb7402c6db89da53d0ca2d0
Roman Kuzmich <rvku2mich@yandex.ru>
2026-01-30 13:52:37 +0500

    @EnableRetry annotation added in WalletServiceApplication

--- src/main/java/ru/kuzmich/walletservice/WalletServiceApplication.java
+++ src/main/java/ru/kuzmich/walletservice/WalletServiceApplication.java
@@ -2,8 +2,10 @@ package ru.kuzmich.walletservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class WalletServiceApplication {
 
   public static void main(String[] args) {
