a7cfa982e3d91f8951b8417df0adc28d9f9ff360
Roman Kuzmich <rvku2mich@yandex.ru>
2026-01-30 23:22:06 +0300

    application config update

--- src/main/java/ru/kuzmich/walletservice/config/DatabaseConfig.java
+++ src/main/java/ru/kuzmich/walletservice/config/DatabaseConfig.java
@@ -1,84 +1,97 @@
-//package ru.kuzmich.walletservice.config;
-//
-//import com.zaxxer.hikari.HikariDataSource;
-//import java.util.Properties;
-//import javax.sql.DataSource;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.orm.jpa.JpaTransactionManager;
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-//import org.springframework.transaction.PlatformTransactionManager;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//
-//@Configuration
-//@EnableTransactionManagement
-//public class DatabaseConfig {
-//
-//  @Value("${spring.datasource.url}")
-//  private String dbUrl;
-//
-//  @Value("${spring.datasource.username}")
-//  private String dbUsername;
-//
-//  @Value("${spring.datasource.password}")
-//  private String dbPassword;
-//
-//  @Value("${spring.datasource.hikari.maximum-pool-size:20}")
-//  private int maxPoolSize;
-//
-//  @Value("${spring.datasource.hikari.minimum-idle:5}")
-//  private int minIdle;
-//
-//  @Value("${spring.datasource.hikari.connection-timeout:30000}")
-//  private int connectionTimeout;
-//
-//  @Bean
-//  public DataSource dataSource() {
-//    HikariDataSource dataSource = new HikariDataSource();
-//    dataSource.setJdbcUrl(dbUrl);
-//    dataSource.setUsername(dbUsername);
-//    dataSource.setPassword(dbPassword);
-//    dataSource.setMaximumPoolSize(maxPoolSize);
-//    dataSource.setMinimumIdle(minIdle);
-//    dataSource.setConnectionTimeout(connectionTimeout);
-//    dataSource.setPoolName("WalletServicePool");
-//    dataSource.setAutoCommit(false);
-//
-//    dataSource.addDataSourceProperty("reWriteBatchedInserts", "true");
-//    dataSource.addDataSourceProperty("prepStmtCacheSize", "250");
-//    dataSource.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-//
-//    return dataSource;
-//  }
-//
-//  @Bean
-//  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-//    em.setDataSource(dataSource());
-//    em.setPackagesToScan("ru.kuzmich.walletservice.model");
-//
-//    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//    em.setJpaVendorAdapter(vendorAdapter);
-//
-//    Properties properties = new Properties();
-//    properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-//    properties.put("hibernate.jdbc.batch_size", "20");
-//    properties.put("hibernate.order_inserts", "true");
-//    properties.put("hibernate.order_updates", "true");
-//    properties.put("hibernate.generate_statistics", "true");
-//    properties.put("hibernate.connection.handling_mode", "DELAYED_ACQUISITION_AND_HOLD");
-//    properties.put("hibernate.connection.provider_disables_autocommit", "true");
-//
-//    em.setJpaProperties(properties);
-//    return em;
-//  }
-//
-//  @Bean
-//  public PlatformTransactionManager transactionManager() {
-//    JpaTransactionManager transactionManager = new JpaTransactionManager();
-//    transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-//    return transactionManager;
-//  }
-//}
+package ru.kuzmich.walletservice.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Value("${spring.datasource.hikari.maximum-pool-size:20}")
+    private int maxPoolSize;
+
+    @Value("${spring.datasource.hikari.minimum-idle:5}")
+    private int minIdle;
+
+    @Value("${spring.datasource.hikari.connection-timeout:30000}")
+    private int connectionTimeout;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        dataSource.setMaximumPoolSize(maxPoolSize);
+        dataSource.setMinimumIdle(minIdle);
+        dataSource.setConnectionTimeout(connectionTimeout);
+        dataSource.setPoolName("WalletServicePool");
+        dataSource.setAutoCommit(false);
+
+        dataSource.addDataSourceProperty("reWriteBatchedInserts", "true");
+        dataSource.addDataSourceProperty("prepStmtCacheSize", "250");
+        dataSource.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        return dataSource;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource());
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+        liquibase.setDefaultSchema("public");
+        liquibase.setShouldRun(true);
+        return liquibase;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("ru.kuzmich.walletservice.model");
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(false);
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.jdbc.batch_size", "20");
+        properties.put("hibernate.order_inserts", "true");
+        properties.put("hibernate.order_updates", "true");
+        properties.put("hibernate.generate_statistics", "true");
+        properties.put("hibernate.connection.handling_mode", "DELAYED_ACQUISITION_AND_HOLD");
+        properties.put("hibernate.connection.provider_disables_autocommit", "true");
+        properties.put("hibernate.hbm2ddl.auto", "validate");
+
+        em.setJpaProperties(properties);
+        return em;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(
+        LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+}
