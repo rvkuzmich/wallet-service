32b7a37ae8658cc3e20ff67411b70254c3b5c8f3
Roman Kuzmich <rvku2mich@yandex.ru>
2026-01-30 16:29:49 +0500

    pom.xml updates

---
+++ src/test/java/ru/kuzmich/walletservice/controller/WalletControllerTest.java
@@ -0,0 +1,7 @@
+package ru.kuzmich.walletservice.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletControllerTest {
+
+}
---
+++ src/test/java/ru/kuzmich/walletservice/controller/WalletIntegrationTest.java
@@ -0,0 +1,5 @@
+package ru.kuzmich.walletservice.controller;
+
+public class WalletIntegrationTest {
+
+}
