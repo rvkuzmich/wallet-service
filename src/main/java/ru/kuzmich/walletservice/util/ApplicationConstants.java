01c05939ce537af8c15a04692206bc52308f674d
Roman Kuzmich <rvku2mich@yandex.ru>
2026-01-30 13:31:50 +0500

    ApplicationConstants added

--- src/main/java/ru/kuzmich/walletservice/util/ApplicationConstants.java
+++ src/main/java/ru/kuzmich/walletservice/util/ApplicationConstants.java
@@ -1,5 +1,9 @@
 package ru.kuzmich.walletservice.util;
 
 public class ApplicationConstants {
-
+  public static final String API_VERSION = "/api/v1";
+  public static final String WALLET_ID = "walletId";
+  public static final String WALLET_PATH = "/wallet";
+  public static final String WALLETS_ID_PATH = "/wallets/{walletId}";
+  public static final String HEALTH_PATH = "/health";
 }
